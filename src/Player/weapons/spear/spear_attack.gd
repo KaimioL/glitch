0d94e64c368985fb0e93a13d7789a12bdf024cf5
Lassi Kaimio <lassi@kaimio.fi>
2023-05-11 14:57:25 +0300

    Add flier enemy and make spear collide with it

--- src/Player/weapons/spear/spear_attack.gd
+++ src/Player/weapons/spear/spear_attack.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
+var _active_frame: bool = false
+
 func _process(delta):
-	pass
+	if(_active_frame):
+		for hitcheck in $Hitchecks.get_children():
+			if(hitcheck.is_colliding()):
+				_active_frame = false
+				get_parent().do_collision_launch(hitcheck.get_collision_normal())
 
 func attack() -> void:
 	$Effect.visible = true
 	$AnimationPlayer.play("attack")
+	_active_frame = true
 	get_parent().start_attack_cooldown()
-	for hitcheck in $Hitchecks.get_children():
-		print(hitcheck.is_colliding())
-		if(hitcheck.is_colliding()):
-			get_parent().do_collision_launch(hitcheck.get_collision_normal())
 
 func _on_animation_player_animation_finished(anim_name):
 	$Effect.visible = false
+	_active_frame = false
---
+++ src/enemies/flier.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _ready():
+	$AnimationPlayer.play("idle")
+
+
+func _process(delta):
+	pass
